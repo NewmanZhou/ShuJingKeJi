462bfc75cd875ab730799befdb5f729141946834
NewmanZhou <fazhuolanyi@163.com>
2018-10-08 22:00:13 +0800

    modify:字段描述

--- dataStructure/semanticscholar.py
+++ dataStructure/semanticscholar.py
@@ -3,9 +3,20 @@
 # semanticscholar 网站采集 数据结构：
 
 semanticscholarItme = {
-    '':'',
-    '':'',
-    '':'',
-    '':'',
-    '':'',
+    'paper_id': '源文件自带',
+    'scholar_id': '源文件自带',
+    'search_title': '要搜索的标题',
+    'authors':['作者数组'],
+    'years':'年份',
+    'journal':'发表期刊/会议',
+    'DOI':'DOI',
+    'topics':['topics'],
+    'contentUrl':'详情页Url',
+    'list_title':'列表标题',
+    'con_title':'正文标题',
+    'list_content':'列表摘要',
+    'con_content':'正文摘要',
+    'title_similarity':'标题相似度（列表标题和原文标题）',
+    'abstract_similarity':'摘要相似度（列表摘要和原文摘要）',
+    'type': 0,
 }
