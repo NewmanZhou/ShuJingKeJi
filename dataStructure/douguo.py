2c6fb8d3fac326bf1cf0f01ddddf12876e058c1f
NewmanZhou <fazhuolanyi@163.com>
2019-01-09 11:22:51 +0800

    modify: 完善豆果美食采集字段说明

--- dataStructure/douguo.py
+++ dataStructure/douguo.py
@@ -21,11 +21,15 @@ douguoItems = {
     'dishes': '荷包蛋面条',  # 菜品名称
     'dishesImg': 'https://cp1.douguo.com/upload/caiku/9/e/9/400x266_9e2a3a7817ade66010ac1de26adee019.jpg',  # 菜品的展示图链接
     'dishesID': '1054561',  # 菜品ID
+    'dishesUrl': '1054561.html',  # 菜品链接
     'browse': '463500',  # 浏览数
     'collect': '10383',  # 收藏数
     'userName': 'lin悠然11',  # 发布者
+    'userID':'', # 发布者ID
+    'userUrl':'', # 发布者链接
     'userImg': 'https://tx1.douguo.com/upload/photo/e/b/d/70_u153299177777809300935.jpg',  # 发布者头像地址
     # 第三部分
+    'abstract':'', # 简介
     'materialList': [
         {'material': '面条', 'number': '400g'},
         {'material': '小葱', 'number': '4根'},
@@ -49,5 +53,10 @@ douguoItems = {
          'img': 'https://cp1.douguo.com/upload/caiku/a/6/2/yuan_a6fd7f2e35349e63c0be4c1416954822.jpg'},
     ],  # 烹饪流程
     'tips': '酱烧葱花油就是将葱花放入碗中，倒入适量生抽，如果想要颜色深可用老抽，然后将油烧热，浇进去即可。',  # 小贴士
-    'classify': ['面条', '主食', '孕妇', '晚餐', '锅具'],  # 分类
+    'classify': ['面条', '主食', '孕妇', '晚餐', '锅具'],  # 分类 + 链接
+    'relatedMenu':['ID+链接'], # 相关菜谱ID + 链接
+    'crawlTime':'', # 抓取时间
+    'content':'菜谱详情原始数据', # 菜谱详情原始数据
+    'source':'豆果美食', # 来源 豆果美食
+    'type':0, # 状态值
 }
